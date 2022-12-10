166b1e13a8c6d6aa7a048fd4f31dc732e45ff711
Martin <mjuliom_4@hotmail.com>
2022-12-10 20:00:01 -0300

    Agregando state pattern

---
+++ src/main/java/state/PlayingState.java
@@ -0,0 +1,36 @@
+package state;
+
+public class PlayingState implements AudioPlayerState {
+
+
+    public void playNextSong(AudioPlayer player) {
+        System.out.println("Playing next song...");
+    }
+
+    public void playPreviousSong(AudioPlayer player) {
+        System.out.println("Playing previous song...");
+    }
+
+    public void startPlayback(AudioPlayer player) {
+        System.out.println("Playback already in progress.");
+    }
+
+    public void stopPlayback(AudioPlayer player) {
+        System.out.println("Stopping playback...");
+        player.setState(new ReadyState());
+    }
+
+    public void lock(AudioPlayer player) {
+        System.out.println("Locking audioplayer.");
+        player.setState(new LockedState());
+    }
+
+    public void unlock(AudioPlayer player) {
+        System.out.println("Doing nothing.. Player is not locked...");
+    }
+
+    @Override
+    public String getName() {
+        return "Playing";
+    }
+}
