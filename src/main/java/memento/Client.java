7696815a9ba2c44004fac9f4a5b894ef63f202bd
Martin <mjuliom_4@hotmail.com>
2022-12-11 10:50:40 -0300

    Memento

---
+++ src/main/java/memento/Client.java
@@ -0,0 +1,29 @@
+package memento;
+
+public class Client {
+
+
+    public static void main(String[] args) {
+
+
+        TextEditorManager textEditorManager = new TextEditorManager(new TextEditor("Design patterns", "Arial", 6));
+
+        textEditorManager.save();
+        textEditorManager.appendText(". Elements of Reusable Object-Oriented Software (1994) is a software engineering book describing software design patterns.");
+        textEditorManager.printState();
+        textEditorManager.undo();
+        textEditorManager.printState();
+
+        textEditorManager.setFontName("Calibri");
+        textEditorManager.setFontSize(9);
+        textEditorManager.save();
+        textEditorManager.printState();
+        textEditorManager.clearText();
+        textEditorManager.printState();
+        textEditorManager.undo();
+        textEditorManager.printState();
+
+    }
+
+
+}
