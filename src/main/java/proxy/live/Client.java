5393bc0f9ffc49d3ad91d32f30c715e4520645b6
Martin <mjuliom_4@hotmail.com>
2023-05-20 09:56:23 -0300

    limpiando

--- src/main/java/proxy/live/Client.java
+++ src/main/java/proxy/live/Client.java
@@ -6,6 +6,11 @@ public class Client {
 
     public static void main(String[] args) {
 
+        //Queremos modifivar el postRepository para que loguie el tiempo de ejecucion del request
+        //No queremos tocar su codigo fuente, (separacion de responsabilidades)
+        //O no podemos (si es una libreria)
+        //Queremos que sea facil volver a la version sin logging
+
         PostRepositoryImpl postRepository = new PostRepositoryImpl();
         Service service = new Service(postRepository);
         service.procesar();
