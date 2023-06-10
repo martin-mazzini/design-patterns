18c4ebcaed58a9540f9756ce0fd95a21ad8da63b
Martin <mjuliom_4@hotmail.com>
2023-06-10 08:45:56 -0300

    add state

--- src/main/java/state/live/Client.java
+++ src/main/java/state/live/Client.java
@@ -1,2 +1,28 @@
-package state.live;public class Client {
+package state.live;
+
+import state.audipayer.AudioPlayer;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        //Queremos programar un reproductor de audio (mp3, parlante, celular)
+        //Puede tener 3 estados. Reproduciendo / Bloqueado / Pausado
+        //La lógica va a variar dependiendo del estado en el que se encuentre, pero los metodos siempre son los mismos
+        //Reproducir, proximaCancion, reproducir, pausarReproduccion, bloquear, desbloquear,
+
+
+
+
+
+
+
+
+
+
+
+        //open closed (vamos a tener que tener muchisimos ifs). Queremos que agregar nuevos estados sea facil.
+        //single responsability (sino se convierte en una clase con muchisima logica). Estaria bueno leer cada estado x separado.
+
+    }
 }
